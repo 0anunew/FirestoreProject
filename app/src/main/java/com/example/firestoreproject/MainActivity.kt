314606aa7bfc6fc2f0bd99cf4cb1cf8f8a749bbd
0anunew <0anunew@gmail.com>
2025-09-27 10:45:07 +0700

    changes

--- app/src/main/java/com/example/firestoreproject/MainActivity.kt
+++ app/src/main/java/com/example/firestoreproject/MainActivity.kt
@@ -11,14 +11,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 class MainActivity : AppCompatActivity() {
 
     private lateinit var titleText: EditText
-    private lateinit var descirptionText: EditText
+    private lateinit var descriptionText: EditText
     private lateinit var textViewData: TextView
     private lateinit var saveButton: Button
     private lateinit var loadButton: Button
     private val dbStore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    private val KEY_TITLE = "title"
-    private val KEY_DESCRIPTION = "description"
+    private val TITLE = "title"
+    private val DESCRIPTION = "description"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         titleText = findViewById(R.id.edit_title)
-        descirptionText = findViewById(R.id.edit_description)
+        descriptionText = findViewById(R.id.edit_description)
         saveButton = findViewById(R.id.button_save)
         textViewData = findViewById(R.id.text_view_data)
         loadButton = findViewById(R.id.button_load)
@@ -48,10 +48,10 @@ class MainActivity : AppCompatActivity() {
     }
     fun saveData() {
         val title = titleText.text.toString()
-        val description = descirptionText.text.toString()
+        val description = descriptionText.text.toString()
         val docData = mutableMapOf<String, Any>(
-            KEY_TITLE to title,
-            KEY_DESCRIPTION to description
+            TITLE to title,
+            DESCRIPTION to description
         )
         dbStore.collection("Collection").document("First Document").set(docData).addOnSuccessListener {
             Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
@@ -63,9 +63,10 @@ class MainActivity : AppCompatActivity() {
     fun loadData(){
         dbStore.collection("Collection").document("First Document").get().addOnSuccessListener {
             if (it != null) {
-                val title = it.getString(KEY_TITLE)
-                val description = it.getString(KEY_DESCRIPTION)
-                textViewData.text = "Title: $title\nDescription: $description"
+                val title = it.getString(TITLE)
+                val description = it.getString(DESCRIPTION)
+                val loadText = "Title: $title\nDescription: $description"
+                textViewData.text = loadText
             }
         }.addOnFailureListener {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
